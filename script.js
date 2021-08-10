0aa9b25accd6208c77546bb3e211c4b37f472a5d
seandaniel <degroot.sd@gmail.com>
2021-08-10 19:41:54 -0400

    Date and Price formatting

--- script.js
+++ script.js
@@ -4,16 +4,34 @@ const app = {};
 
 app.init = () => {
   fetch("https://api.coincap.io/v2/assets/bitcoin/history?interval=d1").then(function(response) {
-    // promise
-    return response.json();
+    if (response.ok) {
+      // returns a ReadableStream that convert to JSON
+      return response.json();
+    } else {
+      // return a rejected promise object that triggers the catch() method
+      return Promise.reject(response);
+    }
   })
     .then(function(response) {
-      console.log(response.data);
 
-      const lastArrayItem = response.data.length - 1
-      console.log(response.data[728])
+      const weeksValues = response.data.slice(-7);
+      console.log(`7 day response:`, weeksValues)
       // map through the last 7 days
       // put priceUSd and date into arrays
+
+      const prices = weeksValues.map(day => {
+        // turn into a number, make to two decimal places, add commas
+        return parseFloat(day.priceUsd).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      });
+
+      const date = weeksValues.map(date => {
+        return date.date.slice(0, 10);
+      });
+      
+      console.log(date, prices);
+
+    }).catch(function(error) {
+      console.log(`API Failed ${error}`);
     });
 };
 
